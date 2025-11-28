6f64dde1e9b49d1298de963c9a07554262c30bcd
MUNSEUNGGEON <smun438@gmail.com>
2025-11-28 16:42:31 +0900

    Tighten DataGrid row spacing

--- Novacos_AIManager/ViewModel/Queries/UserQueries.cs
+++ Novacos_AIManager/ViewModel/Queries/UserQueries.cs
@@ -6,8 +6,8 @@
     public static class UserQueries
     {
         public const string UserList =
-            "SELECT tul_id AS Id, TUL_USER_ID AS UserId, TUL_USER_NAME AS UserName, TUL_USER_EMAIL AS Email, " +
-            "TUL_USER_TYPE AS UserType, TUL_USER_DEPT AS Department, TUL_USER_POSITION AS Position, TUL_CREATED_AT AS CreatedAt" +
+            "SELECT tul_id AS 번호, TUL_USER_ID AS 사용자아이디, TUL_USER_NAME AS 사용자명, TUL_USER_EMAIL AS 이메일, " +
+            "TUL_USER_TYPE AS 사용자유형, TUL_USER_DEPT AS 부서, TUL_USER_POSITION AS 직급, TUL_CREATED_AT AS 등록일 " +
             "FROM tbl_user_list ORDER BY tul_id";
     }
 }
